788b6663907b990dcb22763853e4cc680ea6a449
Adrian Lois <34519511+adrianlois@users.noreply.github.com>
2020-01-08 20:23:03 +0100

    id_4625.ps1

--- id_4625.ps1
+++ id_4625.ps1
@@ -7,6 +7,7 @@ $asuntoEmail = "ID 4625: Inicio de sesion incorrecto"
 
 # Muestra el último evento en formato text con identificador 4625 encadena la salida filtrando por unas palabras clave y redirecciona la salida a un fichero temporal.
 $wevtutil = wevtutil query-events Security /count:1 /rd:true /format:text /q:"Event[System[(EventID=4625)]]" | findstr /V "puerto" | findstr "cuenta: trabajo: Direcci" > "$pathTempWevtutil"
+
 # Lee las últimas 5 líneas del fichero temporal y lo convierte en una cadena de caracteres tal cual se mostrarían en pantalla (formato columna). 
 $cuerpoEmail = Get-Content "$pathTempWevtutil" -Tail 5 | Out-String
 
